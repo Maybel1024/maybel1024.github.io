b9a327652d46b668ff2ded3b6c07815134213813
Maybel1024 <cadetmgallardo@nomma.net>
2022-06-27 17:00:12 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -22,20 +22,21 @@ var init = function (window) {
         // TODO 1 : Declare and initialize our variables
         var circle;			// variable to hold a single circle when creating circles / iterating
         var circles = [];	// variable to store all circles in one Array
-        var drawCircle; // Code to draw a circle
+       
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle() { 
         circle = draw.randomCircleInArea(canvas, true, true, '#100', 2);
         physikz.addRandomVelocity(circle, canvas);
         view.addChild(circle);
         circles.push(circle);
+        }
+        
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
+        for(var i = 0; i < 100; i++){
+            drawCircle(); 
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -48,21 +49,25 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
- 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
- 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
- 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
- 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+            physikz.updatePosition(circle[0]);
+ 	        physikz.updatePosition(circle[1]);
+ 	        physikz.updatePosition(circle[2]);
+ 	        physikz.updatePosition(circle[3]);
+ 	        physikz.updatePosition(circle[4]);
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition(circle[0]);
+            game.checkCirclePosition(circle[1]);
+            game.checkCirclePosition(circle[2]);
+            game.checkCirclePosition(circle[3]);
+            game.checkCirclePosition(circle[4]);
 
             // TODO 9 : Iterate over the array
-           
+            for(var i = 0; i < 100; i++){
+                game.checkCirclePosition(circle[i]);
+                physikz.updatePosition(circle[i]);
+            }
+    
             
         }
     
@@ -77,7 +82,12 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
+         // if the circle has gone past the LEFT side of the screen then place it on the RIGHT
+
+            if ( circle.x > canvas.height) {
+                circle.y = 0;
+            }
+
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
 
