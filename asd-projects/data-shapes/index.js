d8cf5e71c6e45cb744999821b674c3cf8693e7ba
Maybel1024 <cadetmgallardo@nomma.net>
2023-06-06 20:51:03 +0000

    datashapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -38,16 +38,39 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
 
   // TODO 1: create a new shape object and add it to the array
-  
-
+  var shape = {
+    color: "blue",
+    shape: "color",
+    repeat: 3, 
+  }
+dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
-  
+  for(var i = 0; i < dataShapes.length; i++) {
+    
+    var currentShapes = dataShapes[i]
+    if (currentShapes.color === "red"){
+      currentShapes.goodBehavior = "bounce"
+    }
+    else if (currentShapes.color === "blue"){
+      currentShapes.goodBehavior = "blink"
+    }
+    else {
+      currentShapes.goodBehavior = "spin"
+    }
+  }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic (data) {
+    setBackgroundWithObject(data)
+    animationDetails.displayType = 1
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat){
+    setBackgroundWithSimple(color, shape, repeat)
+    animationDetails.displayType = 2 
+  }
+
 
   // TODO 5-a: add a function that handles the bad display type
   
@@ -58,17 +81,17 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes)
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShapes 
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+     
   }
 
   /////////////////////////////////////////////////
